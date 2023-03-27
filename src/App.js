56ff7708e7961e92bcc24491c8efe72be4e10e80
ruben <rubenro2331997@gmail.com>
2023-03-27 11:12:48 +0200

    agrego cosas nuevas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+    <header>
+      <div className='header'> <img src='../imagenes/logo - white.png' className='logo'></img> </div>
+    
+      <div>
+        <img src='../imagenes/header.png'></img>
+      </div>
+    </header>
+    
+    </html>
   );
 }
 
